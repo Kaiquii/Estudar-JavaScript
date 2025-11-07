5a3e91e0ec181e5874ad9918f0b979dc38f6fb99
Kaiqui <kaiqui.lucaskaiquiluc@gmail.com>
2025-11-06 21:54:23 -0300

    Estudando Map

--- Map, Reduce e Filter/script.js
+++ Map, Reduce e Filter/script.js
@@ -20,15 +20,92 @@ const numeros = [1, 2, 3, 4, 5];
 // });
 
 const produtos = [
-  { id: 1, nome: "Samsung Galaxy S21", preco: 3999.99, temDesconto: true, quantidade: 1 },
-  { id: 2, nome: "iPhone 14", preco: 6499.90, temDesconto: false, quantidade: 1 },
-  { id: 3, nome: "Xiaomi Redmi Note 13 Pro", preco: 2299.99, temDesconto: true, quantidade: 1 },
-  { id: 4, nome: "Motorola Edge 40", preco: 2999.00, temDesconto: false, quantidade: 1 },
-  { id: 5, nome: "Samsung Galaxy A54", preco: 2499.90, temDesconto: true, quantidade: 1 },
-  { id: 6, nome: "Realme 11 Pro+", preco: 2799.99, temDesconto: true, quantidade: 1 },
-  { id: 7, nome: "Asus Zenfone 10", preco: 4599.00, temDesconto: false, quantidade: 1 },
-  { id: 8, nome: "Google Pixel 8", preco: 5599.99, temDesconto: true, quantidade: 1 },
-  { id: 9, nome: "OnePlus 11", preco: 4899.00, temDesconto: false, quantidade: 1 },
-  { id: 10, nome: "Poco X6 Pro", preco: 2099.90, temDesconto: true, quantidade: 1 }
+  {
+    id: 1,
+    nome: "Samsung Galaxy S21",
+    preco: 3999.99,
+    temDesconto: true,
+    quantidade: 1,
+  },
+  {
+    id: 2,
+    nome: "iPhone 14",
+    preco: 6499.9,
+    temDesconto: false,
+    quantidade: 1,
+  },
+  {
+    id: 3,
+    nome: "Xiaomi Redmi Note 13 Pro",
+    preco: 2299.99,
+    temDesconto: true,
+    quantidade: 1,
+  },
+  {
+    id: 4,
+    nome: "Motorola Edge 40",
+    preco: 2999.0,
+    temDesconto: false,
+    quantidade: 1,
+  },
+  {
+    id: 5,
+    nome: "Samsung Galaxy A54",
+    preco: 2499.9,
+    temDesconto: true,
+    quantidade: 1,
+  },
+  {
+    id: 6,
+    nome: "Realme 11 Pro+",
+    preco: 2799.99,
+    temDesconto: true,
+    quantidade: 1,
+  },
+  {
+    id: 7,
+    nome: "Asus Zenfone 10",
+    preco: 4599.0,
+    temDesconto: false,
+    quantidade: 1,
+  },
+  {
+    id: 8,
+    nome: "Google Pixel 8",
+    preco: 5599.99,
+    temDesconto: true,
+    quantidade: 1,
+  },
+  {
+    id: 9,
+    nome: "OnePlus 11",
+    preco: 4899.0,
+    temDesconto: false,
+    quantidade: 1,
+  },
+  {
+    id: 10,
+    nome: "Poco X6 Pro",
+    preco: 2099.9,
+    temDesconto: true,
+    quantidade: 1,
+  },
 ];
 
+const novosProdutos = produtos.map((produto) => {
+  const novoPreco = produto.temDesconto ? produto.preco * 0.9 : produto.preco;
+
+  // Ternário = IF e ELSE    ? = IF / ELSE
+
+  return {
+    id: produto.id,
+    nome: produto.nome,
+    preco: novoPreco.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    }),
+    quantidade: produto.quantidade,
+  };
+});
+
+console.log(novosProdutos);
